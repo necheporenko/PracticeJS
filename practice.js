085a70ab548901b8d438cbbb63bc814dbae618c4
whywillywhy <ne4eporenko.v@gmail.com>
2016-08-21 19:48:57 +0300

    update

--- practice.js
+++ practice.js
@@ -470,6 +470,7 @@ calc.read();
 console.log(calc.sum());
 console.log(calc.mul());
 //////////////////////////////////////////////////////////////////////
+Rock, paper or scissors?
 
 var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
@@ -505,4 +506,52 @@ var compare = function(choice1, choice2) {
    }
 }
 compare(userChoice,computerChoice);
+//////////////////////////////////////////////////////////////////////
+
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function printPerson(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+
+function list() {
+	var contactsLength = contacts.length;
+	for (var i = 0; i < contactsLength; i++) {
+		printPerson(contacts[i]);
+	}
+}
+
+function search(lastName){
+    var contactsLength = contacts.length;
+    for (var i = 0; i < contactsLength; i++){       
+        if( lastName == contacts[i].lastName){
+            printPerson(contacts[i]);
+            };
+        };
+    };
+search('Jones');
+
+function add(firstName, lastName, email, phoneNumber) {
+    contacts[contacts.length] = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    phoneNumber: phoneNumber  
+    };
+   };
+add('vasya', 'vasyaa', 'y@com', 5664564);
+list();
 //////////////////////////////////////////////////////////////////////
